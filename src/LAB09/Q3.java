f43585bb42798f7265c3723b7aef7913ae8cff3c
User <xinjie6016@gmail.com>
2023-07-05 03:42:22 +0800

    update

--- src/LAB09/Q3.java
+++ src/LAB09/Q3.java
@@ -2,10 +2,9 @@ package LAB09;
 
 public class Q3 {
     public static void main(String[] args) {
-        System.out.println(exponent(0, 1));
+        System.out.println(exponent(2, 1));
     }
     public static long exponent(int x, int m){
-//        if (m == 1) return x;
         if (m == 0) return 1;
         return x * exponent(x, m-1);
     }
